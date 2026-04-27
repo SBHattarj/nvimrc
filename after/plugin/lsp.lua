4b84476451b3f3236f23e243cc73dbd33bc33543
SBHattarj <subhambhsubhambh@gmail.com>
2026-04-27 10:08:02 +0530

    removed comments from lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -38,28 +38,9 @@ require('mason-lspconfig').setup({
         end,
     }
 })
---for _, lsp in ipairs(require('mason-lspconfig').get_available_servers()) do
---    vim.lsp.config(lsp, {
---        capabilities = lsp_capabilities
---    })
---    vim.lsp.enable(lsp)
---end
-
 local lsp = require('lsp-zero').preset({
 })
 
---lsp.on_attach(function(client, bufnr)
---  lsp.default_keymaps({buffer = bufnr, omit = {'<C-o>', '<C-p>', '<C-P>', '<C-y>'}})
---end)
----- (Optional) Configure lua language server for neovim
-
---lsp.ensure_installed({
---	'ts_ls',
---	'eslint',
---    'svelte',
---	'rust_analyzer',
---	'lua_ls'
---})
 
 local cmp = require('cmp')
 cmp.setup({
@@ -84,18 +65,4 @@ lsp.setup_nvim_cmp({
 	mapping = cmp_mappings
 })
 
--- lsp.on_attach(function(client, bufnr)
---  local opts = {buffer = bufnr, remap = false}
-
---  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
---  vim.keymap.set("n", "K", function() vim.lsp.hover() end, opts)
---  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
---  vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
---  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
---  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
---  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
---  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.reference() end, opts)
---  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
---  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
---end)
 lsp.setup()
